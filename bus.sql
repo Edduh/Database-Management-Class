11891f0cda6bbd7da65221a36e830e8357dd75e4
Edward Su <74035257+Edduh@users.noreply.github.com>
2020-11-16 14:47:12 -0500

    Create bus.sql

---
+++ bus.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS bus_info
+(
+busID INT NOT NULL,
+employeeID  INT NOT NULL,
+credit_card VARCHAR(255) NOT NULL,
+PRIMARY KEY (busID),
+FOREIGN KEY (employeeID) REFERENCES employee_pid(employeeID) 
+);
+
+INSERT INTO bus_info ( busID , employeeID , credit_card )
+( 1 , 1 , 41002311231 ) ,
+( 2 , 2 , 51231231231 ) ,
+( 3 , 3 , 34931823989 ) , 
+( 4 , 4 , 29138074213 ) ,
+( 5 , 5 , 23193812097 ) ; 
