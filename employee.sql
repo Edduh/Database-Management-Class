5ab089ad329b52aa0376f9ae80c14b255dd5d5dc
Edward Su <74035257+Edduh@users.noreply.github.com>
2020-11-16 20:58:40 -0500

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -11,6 +11,7 @@ PRIMARY KEY (employeeID)
 ) ;
 
 INSERT INTO employee_info ( employeeID , license_state , license_number , date_of_birth , social_security , address , date_of_hire )
+VALUES
 ( 1 , "NY" , 445234213 , 19970101 , 083812559 , "3443 Hylan Blvd Staten Island NY 10306" , 20200106 ),
 ( 2 , "NY" , 488281832 , 19660102 , 091232132 , "147 King Street Brooklyn NY 11231" , 20200122 ),
 ( 3 , "PA" , 688912398 , 19550120 , 041238123 , "89 Sackett Street Brooklyn NY 11231" , 20190122 ),
