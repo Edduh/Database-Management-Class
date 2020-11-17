0cbf5702d3196f7b05f1e8d3135b608adb3fd96b
Edward Su <74035257+Edduh@users.noreply.github.com>
2020-11-16 20:59:49 -0500

    Update bus.sql

--- bus.sql
+++ bus.sql
@@ -8,8 +8,14 @@ FOREIGN KEY (employeeID) REFERENCES employee_pid(employeeID)
 );
 
 INSERT INTO bus_info ( busID , employeeID , credit_card )
+VALUES
 ( 1 , 1 , 41002311231 ) ,
 ( 2 , 2 , 51231231231 ) ,
 ( 3 , 3 , 34931823989 ) , 
 ( 4 , 4 , 29138074213 ) ,
 ( 5 , 5 , 23193812097 ) ; 
+
+SELECT * FROM bus_info;
+SELECT busID FROM bus_info;
+SELECT employeeID FROM bus_info;
+SELECT credit_card FROM bus_info;
