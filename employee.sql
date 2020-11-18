ff07a6c1e11e32031014bfd3933e50ab23ba4214
Edward Su <74035257+Edduh@users.noreply.github.com>
2020-11-17 19:46:02 -0500

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -6,7 +6,7 @@ license_number	VARCHAR(32)	NOT NULL,
 date_of_birth	DATE	NOT NULL,
 social_security	VARCHAR(32)	NOT NULL,
 address	VARCHAR(255) NOT NULL,
-date_of_hire	DATE	NOT NULL
+date_of_hire	DATE	NOT NULL,
 PRIMARY KEY (employeeID)
 ) ;
 
