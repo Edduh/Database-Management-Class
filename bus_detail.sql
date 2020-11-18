6c6a4604cb3e21c55d24f679ac4022dd3a564b71
Edward Su <74035257+Edduh@users.noreply.github.com>
2020-11-17 22:01:43 -0500

    Update bus_detail.sql

--- bus_detail.sql
+++ bus_detail.sql
@@ -1,21 +1,24 @@
 CREATE TABLE IF NOT EXISTS bus_detail
 (
-busID INT NOT NULL,
-vin TEXT NOT NULL,
+busID	INT NOT NULL,
+vehicleID	VARCHAR(255) NOT NULL,
+vehicleIN	VARCHAR(255) NOT NULL,
 registration  INT NOT NULL,
 inspection_date  DATE NOT NULL,
-FORIEGN KEY (busID) REFERENCES bus_info (busID)
+FOREIGN KEY (busID) REFERENCES bus_info(busID)
 );
 
-INSERT INTO charter_list ( busID , vin , registration , inspection_date )
+INSERT INTO bus_detail ( busID , vehicleID , vehicleIN , registration , inspection_date )
 VALUES
-( 1 , "YE2TC132BF2044" , 20201120 ),
-( 2 , "YE2TC132BF2044" , 20201020 ),
-( 3 , "YE2TC132BF2044" , 20200816 ),
-( 4 , "YE2TC132BF2044" , 20201222 ),
-( 5 , "YE2TC132BF2044" , 20201016 ):
+( 1 , 333 , "YE2TC132BF2044" , 1997 , 20201120 ),
+( 2 , 343 , "YE2TC132BF2044" , 2000 , 20201020 ),
+( 3 , 363 , "YE2TC132BF2044" , 2006 , 20200816 ),
+( 4 , 383 , "YE2TC132BF2044" , 2003 , 20201222 ),
+( 5 , 393 , "YE2TC132BF2044" , 2007 , 20201016 );
 
 SELECT * FROM bus_detail;
-SELECT vin FROM bus_detail;
+SELECT busID FROM bus_detail;
+SELECT vehicleID FROM bus_detail;
+SELECT vehicleIN FROM bus_detail;
 SELECT registration FROM bus_detail;
 SELECT inspection_date FROM bus_detail;
