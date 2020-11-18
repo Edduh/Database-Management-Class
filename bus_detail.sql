00361420f37efb73336c65c03a4dd60987bdb4a7
Edward Su <74035257+Edduh@users.noreply.github.com>
2020-11-17 21:14:37 -0500

    Create bus_detail.sql

---
+++ bus_detail.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS bus_detail
+(
+busID INT NOT NULL,
+vin TEXT NOT NULL,
+registration  INT NOT NULL,
+inspection_date  DATE NOT NULL,
+PRIMARY KEY (busID)
+);
+
+INSERT INTO charter_list ( busID , vin , registration , inspection_date )
+VALUES
+( 1 , "YE2TC132BF2044" , 20201120 ),
+( 2 , "YE2TC132BF2044" , 20201020 ),
+( 3 , "YE2TC132BF2044" , 20200816 ),
+( 4 , "YE2TC132BF2044" , 20201222 ),
+( 5 , "YE2TC132BF2044" , 20201016 ):
+
+SELECT * FROM bus_detail;
+SELECT vin FROM bus_detail;
+SELECT registration FROM bus_detail;
+SELECT inspection_date FROM bus_detail;
